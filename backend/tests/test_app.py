d886e321de1d6b6c1d9e38d07d8f158b3767377d
rkfcl <kwmkr6@gmail.com>
2024-08-09 05:05:27 +0000

    feat: test_app-py

---
+++ backend/tests/test_app.py
@@ -0,0 +1,17 @@
+# backend/tests/test_app.py
+import unittest
+from app import app
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"message": "Hello, Docker World!"})
+
+if __name__ == "__main__":
+    unittest.main()
